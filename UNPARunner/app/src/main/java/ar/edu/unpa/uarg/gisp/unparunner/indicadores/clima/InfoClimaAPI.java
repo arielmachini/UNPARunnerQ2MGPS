13e88f363c559e7596337f0439c5214ffc8afba8
Ariel Machini <amachini@uarg.unpa.edu.ar>
2021-07-25 12:58:49 -0300

    Coordenadas modificadas.

--- UNPARunner/app/src/main/java/ar/edu/unpa/uarg/gisp/unparunner/indicadores/clima/InfoClimaAPI.java
+++ UNPARunner/app/src/main/java/ar/edu/unpa/uarg/gisp/unparunner/indicadores/clima/InfoClimaAPI.java
@@ -13,13 +13,16 @@ public interface InfoClimaAPI {
     String apiKey = "d459a571ba2de7d1771262a052ba2b1b";
 
     /* Coordenadas de Comandante Luis Piedrabuena: */
-    // double latitud = -49.983, longitud = -68.91;
+    // double latitud = -49.98300, longitud = -68.91000;
 
     /* Coordenadas de Río Gallegos: */
-    double latitud = -51.633333, longitud = -69.233333;
+    double latitud = -51.63333, longitud = -69.23333;
 
     /* Coordenadas de CABA: */
-    // double latitud = -34.6131500, longitud = -58.3772300;
+    // double latitud = -34.61315, longitud = -58.37723;
+
+    /* Coordenadas de San Isidro: */
+    // double latitud = -34.4721, longitud = -58.52708;
 
     String parametros = "?lat=" + latitud + "&lon=" + longitud + "&appid=" + apiKey + "&units=metric&lang=es";
 
